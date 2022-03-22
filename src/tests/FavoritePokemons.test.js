21957ed7a3bd35659cea330d5c81456e13cdc561
ottomicheletti <michelettiotto@gmail.com>
2022-03-22 19:18:09 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import { cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  afterEach(() => {
+    cleanup();
+  });
+  test(`É exibido na tela a mensagem No favorite pokemon found,
+se a pessoa não tiver pokémons favoritos`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText(/no favorite/i);
+    expect(message).toBeInTheDocument();
+  });
+  test('É exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByText(/more details/i);
+    userEvent.click(details);
+    const favoritePokemon = screen.getByRole('checkbox', { checked: false });
+    userEvent.click(favoritePokemon);
+    const favorites = screen.getByText(/favorite/i);
+    userEvent.click(favorites);
+    const cards = screen.getAllByTestId('pokemon-name');
+    const UM = 1;
+    expect(cards.length).toBe(UM);
+  });
+});
